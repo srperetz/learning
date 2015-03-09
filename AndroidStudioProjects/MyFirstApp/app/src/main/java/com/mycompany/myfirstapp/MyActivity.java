5486d42293391389f58227e0e27ca346780c43c7
Sam Peretz <peretz@alum.mit.edu>
2015-03-09 15:24:24 -0400

    my first app files

--- AndroidStudioProjects/MyFirstApp/app/src/main/java/com/mycompany/myfirstapp/MyActivity.java
+++ AndroidStudioProjects/MyFirstApp/app/src/main/java/com/mycompany/myfirstapp/MyActivity.java
@@ -1,12 +1,25 @@
 package com.mycompany.myfirstapp;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-
+import android.view.View;
+import android.widget.EditText;
+import android.content.Intent;
 
 public class MyActivity extends ActionBarActivity {
+
+    public final static String EXTRA_MESSAGE = "com.mycompany.myfirstapp.MESSAGE";
+
+    /** Called when the user clicks the Send button */
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
