0fe8dd375aba91a68b4a0f58f5291d047758d139
Sam Peretz <peretz@alum.mit.edu>
2015-04-28 11:31:24 -0400

    added usage message

--- java/sorting/compare.bat
+++ java/sorting/compare.bat
@@ -1,1 +1,8 @@
+@if "%2"=="" (
+  @echo Usage: %0 A B
+  @echo   where A and B are the two types of sorts to compare,
+  @echo         any two of:  B[ubble], S[election], I[nsertion], M[erge], H[eap] or Q[uick]
+  @goto DONE
+)
 @for %%I in (testfiles\*.txt) DO @diff -q output\%1.%%~nxI output\%2.%%~nxI
+:DONE
